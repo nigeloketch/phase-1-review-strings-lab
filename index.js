bcb8d2112132ef34ff7979e33ed31b1c07cf5b06
Nigel <nigel.oketch@student.moringaschool.com>
2023-06-12 22:12:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser ='Nigel Bahati';
+const WelcomeMessage ='Welcome to Flatbook, ${currentUser}!';
+const excutedWelcomeMessage =WelcomeMessage.toUppercase();
+const shortGreeting ='welcome, {currentUser.slice(0,1)}!';
